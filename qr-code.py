3c983b0d951056426aab1410dc534a28f47b1413
Mostafa Al Mamun Fahim <67799909+pathetic-coder@users.noreply.github.com>
2023-09-06 10:40:38 +0600

    added script

---
+++ qr-code.py
@@ -0,0 +1,29 @@
+import qrcode
+
+class QR_Gen:
+    def __init__(self, size:int, padding:int):
+        self.qrcode = qrcode.QRCode(box_size=size, border=padding)
+
+    def creat_qr(self, f_name:str, fg:str, bg:str):
+        user_input = input("Please enter your text/link: ")
+
+        try:
+            self.qrcode.add_data(user_input)
+            qr_image = self.qr.make_image(fill_color=fg, back_color=bg)
+            qr_image.save(f_name)
+            print(f"Successfull created ({f_name})")
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+
+def main():
+    qr = QR_Gen(size=30, padding=2)
+    qr.creat_qr("sample.png", 
+                fg='black', 
+                bg='white')
+
+
+if __name__=="__main__":
+    main()
